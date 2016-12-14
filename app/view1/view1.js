91c4c8f50f668303b6afe191824919a30f591a9e
Patricio Echague <patricioe@gmail.com>
2016-12-14 13:52:10 -0800

    change module to splitModule

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute', 'thirdParty'])
+angular.module('myApp.view1', ['ngRoute', 'splitModule'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
